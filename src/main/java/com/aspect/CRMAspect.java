9e955e556d9a904b5b1d03d95f34bf884f6a7d86
Dawid <infarot@gmail.com>
2019-01-19 12:38:44 +0100

    added afterFlowAdvice for aop stuff

--- src/main/java/com/aspect/CRMAspect.java
+++ src/main/java/com/aspect/CRMAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public class CRMLoggingAspect {
+public class CRMAspect {
 
     @Pointcut("execution(* com.controller.*.*(..))")
     public void controllerPackage() {
--- src/main/java/com/aspect/FlowAspect.java
+++ src/main/java/com/aspect/FlowAspect.java
@@ -3,10 +3,7 @@ package com.aspect;
 import com.config.MvcConfiguration;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +15,16 @@ public class FlowAspect {
 
     private Logger logger = Logger.getLogger(getClass().getName());
 
-    @Before("com.aspect.CRMLoggingAspect.appFlow()")
+    @Before("com.aspect.CRMAspect.appFlow()")
     public void beforeFlowAdvice(JoinPoint joinPoint){
         Signature signature = joinPoint.getSignature();
         logger.info("Flow before advice! executing method: " + signature.toShortString());
     }
 
-    @After("com.aspect.CRMLoggingAspect.appFlow()")
-    public void afterFlowAdvice(){
+    @AfterReturning(pointcut ="com.aspect.CRMAspect.appFlow()", returning = "result")
+    public void afterFlowAdvice(JoinPoint joinPoint, Object result){
+        logger.info("Flow after returning from method: "+ joinPoint.getSignature().toShortString());
+        logger.info("Result: " + result);
 
     }
 }
